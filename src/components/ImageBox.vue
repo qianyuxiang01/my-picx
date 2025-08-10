4a421f90fe2e4e109fe3670f3ca57854295481ba
qianyuxiang <qianyuxiang@qianyuxiangdeMacBook-Pro.local>
2025-08-10 18:24:17 +0800

    Update: update copy link

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -16,30 +16,30 @@
 			:preview-src-list="[src]"
 		/>
 		<div class="w-full absolute left-0 bottom-0 bg-slate-800/70 backdrop-blur-sm">
-			<div class="p-2">
+			<!-- <div class="p-2">
 				<div class="w-full flex items-center text-white">
 					<div class="flex-1 w-full truncate">
 						<el-tooltip :content="name" placement="top-start">
 							{{ name }}
 						</el-tooltip>
 					</div>
-					<!-- <div
+					<div
 						v-if="mode === 'converted'"
 						class="w-6 h-6 flex items-center justify-center cursor-pointer"
 						@click="emit('delete')"
 					>
 						<font-awesome-icon :icon="faTimesCircle" />
-					</div> -->
+					</div>
 				</div>
-				<!-- <span class="text-xs text-gray-300 flex items-center">
+				<span class="text-xs text-gray-300 flex items-center">
 					{{ formatBytes(size) }}
 					<el-divider v-if="uploadedAt" direction="vertical" />
 					<span v-if="uploadedAt">
 						{{ new Date(uploadedAt).toLocaleDateString() }}
 					</span>
-				</span> -->
-			</div>
-			<!-- <div v-if="mode === 'uploaded'">
+				</span>
+			</div> -->
+			<div v-if="mode === 'uploaded'">
 				<el-divider class="m-0" />
 				<div class="w-full flex text-white h-9 text-center text-sm">
 					<el-tooltip :content="src" placement="top-start">
@@ -48,10 +48,10 @@
 							@click="copyLink(src)"
 						>
 							<font-awesome-icon :icon="faCopy" class="mr-2" />
-							链接
+							链接{{ name }}
 						</div>
 					</el-tooltip>
-					<el-divider direction="vertical" class="h-full" />
+					<!-- <el-divider direction="vertical" class="h-full" />
 					<el-popconfirm
 						title="确认删除图片吗？"
 						confirm-button-type="danger"
@@ -70,9 +70,9 @@
 								删除
 							</div>
 						</template>
-					</el-popconfirm>
+					</el-popconfirm> -->
 				</div>
-			</div> -->
+			</div>
 		</div>
 	</div>
 </template>
