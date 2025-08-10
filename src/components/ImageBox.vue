56438365d91af588cae3f21629e8fe0c1fd1fe20
qianyuxiang <qianyuxiang@qianyuxiangdeMacBook-Pro.local>
2025-08-10 17:53:40 +0800

    Update: add 悬浮文件名

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -16,29 +16,29 @@
 			:preview-src-list="[src]"
 		/>
 		<div class="w-full absolute left-0 bottom-0 bg-slate-800/70 backdrop-blur-sm">
-			<!-- <div class="p-2">
+			<div class="p-2">
 				<div class="w-full flex items-center text-white">
 					<div class="flex-1 w-full truncate">
 						<el-tooltip :content="name" placement="top-start">
 							{{ name }}
 						</el-tooltip>
 					</div>
-					<div
+					<!-- <div
 						v-if="mode === 'converted'"
 						class="w-6 h-6 flex items-center justify-center cursor-pointer"
 						@click="emit('delete')"
 					>
 						<font-awesome-icon :icon="faTimesCircle" />
-					</div>
+					</div> -->
 				</div>
-				<span class="text-xs text-gray-300 flex items-center">
+				<!-- <span class="text-xs text-gray-300 flex items-center">
 					{{ formatBytes(size) }}
 					<el-divider v-if="uploadedAt" direction="vertical" />
 					<span v-if="uploadedAt">
 						{{ new Date(uploadedAt).toLocaleDateString() }}
 					</span>
-				</span>
-			</div> -->
+				</span> -->
+			</div>
 			<!-- <div v-if="mode === 'uploaded'">
 				<el-divider class="m-0" />
 				<div class="w-full flex text-white h-9 text-center text-sm">
